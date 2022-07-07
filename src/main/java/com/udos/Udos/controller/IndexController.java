0de128e562a2d844f20fefaf746e63a04e61ac2b
Niko046 <106796541+Niko046@users.noreply.github.com>
2022-07-07 00:46:27 -0500

    bienvenido

--- src/main/java/com/udos/Udos/controller/IndexController.java
+++ src/main/java/com/udos/Udos/controller/IndexController.java
@@ -15,6 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
     @GetMapping("/")
     public String index(){
-    return ""; 
+    return "Bienvenido al back"; 
     }
 }
